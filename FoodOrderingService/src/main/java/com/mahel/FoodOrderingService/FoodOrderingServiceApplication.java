79f95e9f7106c5b7b387560aceb522a70265b277
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-05-25 12:35:47 +0530

    first commit

---
+++ FoodOrderingService/src/main/java/com/mahel/FoodOrderingService/FoodOrderingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mahel.FoodOrderingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingServiceApplication.class, args);
+	}
+
+}
---
+++ FoodOrderingService/src/test/java/com/mahel/FoodOrderingService/FoodOrderingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahel.FoodOrderingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
