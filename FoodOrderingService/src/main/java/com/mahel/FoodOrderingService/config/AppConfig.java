0fbc1b1272f4a6cf2f16f95d36043282d1d6a3ef
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-05-26 08:59:53 +0530

    implement spring security

---
+++ FoodOrderingService/src/main/java/com/mahel/FoodOrderingService/config/AppConfig.java
@@ -0,0 +1,66 @@
+package com.mahel.FoodOrderingService.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER", "ADMIN")
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        return null;
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+                corsConfiguration.setAllowedOrigins(Arrays.asList(
+                        "http://localhost:3000/"
+                ));
+                corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                corsConfiguration.setAllowCredentials(true);
+                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+                corsConfiguration.setMaxAge(3600L);
+
+                return corsConfiguration;
+            }
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ FoodOrderingService/src/main/java/com/mahel/FoodOrderingService/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.mahel.FoodOrderingService.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "fjskdfjsdkoewrwkmvxcdfslfsdfirwoerwirojfsldfjsfnvlpoqwewqic";
+
+    public static final String JWT_HEADER = "Authorization";
+}
