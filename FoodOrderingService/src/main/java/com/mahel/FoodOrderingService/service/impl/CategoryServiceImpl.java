548736eb29cf9abb0c309173a91513d1d31e7b89
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-03 15:22:39 +0530

    implement category service and catergory service impl

---
+++ FoodOrderingService/src/main/java/com/mahel/FoodOrderingService/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.mahel.FoodOrderingService.service.impl;
+
+import com.mahel.FoodOrderingService.exception.BadRequestException;
+import com.mahel.FoodOrderingService.model.Category;
+import com.mahel.FoodOrderingService.model.Restaurant;
+import com.mahel.FoodOrderingService.repository.CategoryRepository;
+import com.mahel.FoodOrderingService.service.CategoryService;
+import com.mahel.FoodOrderingService.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Override
+    public Category createCategory(String name, Long restaurantId) {
+
+        Restaurant restaurant = restaurantService.findById(restaurantId);
+
+        Category category = new Category();
+        category.setName(name);
+        category.setRestaurant(restaurant);
+
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> findCategoryByRestaurantId(Long id) throws Exception {
+        return categoryRepository.findRestaurantId(id);
+    }
+
+    @Override
+    public Category findCategoryById(Long id) throws Exception {
+
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+
+        if (optionalCategory.isEmpty()) {
+            throw new BadRequestException("Category Not Found");
+        }
+
+        return optionalCategory.get();
+    }
+}
