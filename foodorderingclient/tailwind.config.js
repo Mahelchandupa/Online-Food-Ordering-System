8efd033dff2c2475d215c9f31f8ae87b95b0c96a
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-05 07:47:41 +0530

    implement navbar

--- foodorderingclient/tailwind.config.js
+++ foodorderingclient/tailwind.config.js
@@ -1,9 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      dancing: ["Dancing Script", "cursive"],
+      raleway: ["Raleway", "sans-serif"],
+      inter: ["Inter", "sans-serif"]
+    },
+    colors: {
+      'gray': '#7C6A46',
+      'customBlack': "#1C1C1C",
+      'white': '#FFFFFF',
+      'garyWhite': '#FAFAFA'
+    },
     extend: {},
   },
   plugins: [],
