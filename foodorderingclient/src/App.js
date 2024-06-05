7a7d1c166588a55da8d4156394c5b35cea00c2b2
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-05 11:38:44 +0530

    implement home page header

--- foodorderingclient/src/App.js
+++ foodorderingclient/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
       <div className=" font-poppins">
         <NavBar />
+        <Home />
       </div>
   );
 }
---
+++ foodorderingclient/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import HeaderCarousel from '../../components/HeaderCarousel';
+
+const Home = () => {
+    return (
+        <div>
+            <HeaderCarousel />
+        </div>
+    );
+};
+
+export default Home;
