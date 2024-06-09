383be093c635a058b25a26e9ae4662cf73509dc9
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-09 15:42:38 +0530

    create cart item ui

--- foodorderingclient/src/App.js
+++ foodorderingclient/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Restaurant from './pages/Restaurant/Restaurant';
-import Home from './pages/home/Home';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart'
 
 function App() {
   return (
       <div className=" font-poppins">
         <NavBar />
         {/* <Home /> */}
-        <Restaurant />
+        {/* <Restaurant /> */}
+        <Cart />
       </div>
   );
 }
